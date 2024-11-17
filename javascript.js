bb5069ef9775e645ba3797ab8099cbda98b492fd
Vicktorious23 <90080625+VICKTORIOUS23@users.noreply.github.com>
2024-11-17 07:51:00 -0600

    test

--- javascript.js
+++ javascript.js
@@ -1,3 +1,3 @@
-fetch("menuitems.json")
+fetch("https://github.com/VICKTORIOUS23/odin-restaurant-page/blob/main/menuitems.json")
     .then(response => response.json())
     .then(value => console.log(value))
